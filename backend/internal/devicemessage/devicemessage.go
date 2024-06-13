c94383a63dc1c23759052461b1fce0de7b9c7386
Franklin Junior <franklinjunior12@hotmail.com>
2024-06-13 11:07:35 -0300

    extract header from packet

--- backend/internal/devicemessage/devicemessage.go
+++ backend/internal/devicemessage/devicemessage.go
@@ -1,6 +1,10 @@
 package devicemessage
 
-import "unsafe"
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+)
 
 type Header struct {
 	DeviceId  uint64
@@ -8,6 +12,25 @@ type Header struct {
 	Version   uint32
 }
 
-var headerExample Header
+const HEADERSIZE = 28
 
-const HEADERSIZE = int(unsafe.Sizeof(headerExample))
+func ExtractHeader(packet []byte) (Header, error) {
+	var header Header
+	if len(packet) < HEADERSIZE {
+		return header, errors.New("invalid packet size")
+	}
+	buf := bytes.NewReader(packet[:HEADERSIZE])
+	err := binary.Read(buf, binary.LittleEndian, &header.DeviceId)
+	if err != nil {
+		return header, err
+	}
+	err = binary.Read(buf, binary.LittleEndian, &header.AuthToken)
+	if err != nil {
+		return header, err
+	}
+	err = binary.Read(buf, binary.LittleEndian, &header.Version)
+	if err != nil {
+		return header, err
+	}
+	return header, nil
+}
