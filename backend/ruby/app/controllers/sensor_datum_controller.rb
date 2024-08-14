a78e0d461b4abb9bf52cb0fee35733c12e97a660
Franklin Junior <franklinjunior12@hotmail.com>
2024-08-14 09:59:57 -0300

    chore: Refactor SensorDatumController#index method

--- backend/ruby/app/controllers/sensor_datum_controller.rb
+++ backend/ruby/app/controllers/sensor_datum_controller.rb
@@ -1,14 +1,32 @@
 class SensorDatumController < ApplicationController
   # ignores authentication for now
   def index
-    data = SensorDatum.where(sensor: params[:sensor_id])
-    if data.empty?
-      render json: { error: "No data found for the specified sensor" }, status: :bad_request
-      return
+    if params[:sensor_id].present?
+      data = SensorDatum.where(sensor: params[:sensor_id])
+      if data.empty?
+        render json: { error: "No data found for the specified sensor" }, status: :bad_request
+        return
+      end
+      data = data.pluck(:value, :recorded_at).map do |value, recorded_at|
+        { value: value, recorded_at: recorded_at }
+      end
+      render json: data
+    elsif params[:device_id].present?
+      # will take data by sensor_type
+      data = {}
+      sensors = Sensor.where(device: params[:device_id])
+      if sensors.empty?
+        render json: { error: "No sensors found for the specified device" }, status: :bad_request
+        return
+      end
+      sensors.each do |sensor|
+        data[sensor.sensor_type] = SensorDatum.where(sensor: sensor.id).pluck(:value, :recorded_at).map do |value, recorded_at|
+          { value: value, recorded_at: recorded_at }
+        end
+      end
+      render json: data
+    else
+      render json: { error: "No sensor_id or device_id specified" }, status: :bad_request
     end
-    data = data.pluck(:value, :recorded_at).map do |value, recorded_at|
-      { value: value, recorded_at: recorded_at }
-    end
-    render json: data
   end
 end
