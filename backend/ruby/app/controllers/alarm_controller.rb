ef2522e7095ea62d2206bd06b2091394460e71d1
Franklin Junior <franklinjunior12@hotmail.com>
2024-08-31 16:24:38 -0300

    post alarms

--- backend/ruby/app/controllers/alarm_controller.rb
+++ backend/ruby/app/controllers/alarm_controller.rb
@@ -1,6 +1,8 @@
 class AlarmController < ApplicationController
   # ignores authentication for now
-  before_action :set_cors_headers
+  before_action :set_cors_headers, only: [:index]
+  # skip Can't verify CSRF token authenticity for post requests
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
     if params[:device_id].present?
@@ -15,6 +17,19 @@ class AlarmController < ApplicationController
       render json: data
     else
       render json: { error: "No device_id provided" }, status: :bad_request
+    end
+  end
+
+  def create
+    if params[:device_id].present? && params[:trigger_condition].present? && params[:notification_endpoint].present?
+      alarm = Alarm.new(device_id: params[:device_id], trigger_condition: params[:trigger_condition], notification_endpoint: params[:notification_endpoint])
+      if alarm.save
+        render json: { success: "Alarm created successfully" }
+      else
+        render json: { error: "Failed to create alarm" }, status: :bad_request
+      end
+    else
+      render json: { error: "Missing required parameters" }, status: :bad_request
     end
   end
 
