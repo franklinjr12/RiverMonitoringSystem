5fcc02a0ba78f57afdf9e985d7d2853a827ae547
Franklin Junior <franklinjunior12@hotmail.com>
2025-04-12 13:01:08 -0300

    Refactor WiFi configuration by moving credentials to config.hpp; update HTTP request handling in river_monitor.ino

---
+++ firmware/river_monitor/config.hpp
@@ -0,0 +1,10 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+#define HOST "http://YOUR_HOST:YOUR_PORT"
+#define DATA_POST_PATH "/sensor_datum/create"
+
+#endif
--- firmware/river_monitor/river_monitor.ino
+++ firmware/river_monitor/river_monitor.ino
@@ -1,10 +1,7 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
-
-const char* serverName = "http://192.168.0.26:3000/sensor_datum/create";
+#include "config.hpp"
 
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
@@ -21,7 +18,7 @@ void syncClock() {
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASS);
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -39,11 +36,13 @@ void loop() {
     if(WiFi.status()== WL_CONNECTED){
       WiFiClient client;
       HTTPClient http;
-      http.begin(client, serverName);      
+      char buffer[256];
+      sprintf(buffer, "%s%s", HOST, DATA_POST_PATH);
+      String httpUrl = String(buffer);
+      http.begin(client, httpUrl);
       // If you need Node-RED/server authentication, insert user and password below
       //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
       http.addHeader("Content-Type", "application/json");
-      char buffer[256];
       int sensorId = 1;
       float value = 1.0;
       auto currentTime = time(nullptr);
@@ -51,6 +50,7 @@ void loop() {
       String recodedAt = String(ctime(&currentTime));
       sprintf(buffer, "{\"payload\":[{\"sensor_id\":%d,\"value\":%f,\"recorded_at\":\"%s\"}]}", sensorId, value, recodedAt.c_str());
       String httpRequestData = buffer;
+      Serial.printf("writing %s to %s.", buffer, httpUrl.c_str());
       int httpResponseCode = http.POST(httpRequestData);
       
       Serial.print("HTTP Response code: ");
