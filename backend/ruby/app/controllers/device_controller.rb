b8416dd82d5c5f93df5e5682e2571e24341c691c
Franklin Junior <franklinjunior12@hotmail.com>
2025-12-17 11:32:57 -0300

    small adjusts on controller

--- backend/ruby/app/controllers/device_controller.rb
+++ backend/ruby/app/controllers/device_controller.rb
@@ -9,17 +9,23 @@ class DeviceController < ApplicationController
       return
     end
     devices_data = devices.map do |device|
-      last_read_level = device.sensors.where(sensor_type: :level).first.sensor_data.order(created_at: :desc).last
-      last_read_temperature = device.sensors.where(sensor_type: :temperature).first.sensor_data.order(created_at: :desc).last
-      last_read_at = last_read_level.created_at > last_read_temperature.created_at ? last_read_level.created_at : last_read_temperature.created_at
+      last_read_level = device.sensors&.where(sensor_type: :level)&.first&.sensor_data&.order(created_at: :desc)&.first
+      last_read_temperature = device.sensors&.where(sensor_type: :temperature)&.first&.sensor_data&.order(created_at: :desc)&.first
+      last_read_at = if last_read_level.present? && last_read_temperature.present?
+        last_read_level.created_at > last_read_temperature.created_at ? last_read_level.created_at : last_read_temperature.created_at
+      elsif last_read_level.present?
+        last_read_level
+      elsif last_read_temperature.present?
+        last_read_temperature
+      end
       {
         id: device.id,
         name: device.name,
         location: device.location,
         last_read_at: last_read_at,
-        last_level: last_read_level.value,
-        last_temperature: last_read_temperature.value,
-        status: device_status(last_read_at)
+        last_level: last_read_level&.value&.round(2),
+        last_temperature: last_read_temperature&.value&.round(2),
+        status: last_read_at.present? ? device_status(last_read_at) : :offline
       }
     end
     render json: devices_data
